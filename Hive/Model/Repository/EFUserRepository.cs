19bcca5ecdc04e2fc7d2e68123ac8dc90d2f0f69
Grzegorz Ligas <ligasgr@gmail.com>
2012-11-07 10:30:29 +0100

    Initial Model and WpfGui stubs

---
+++ Hive/Model/Repository/EFUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model.Repository
+{
+    class EFUserRepository : UserRepository
+    {
+        public void VerifyUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
