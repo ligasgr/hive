570f4318efb49e39972fd2d0b364a979ca170047
Grzegorz Ligas <ligasgr@gmail.com>
2012-11-09 09:27:39 +0100

    Moving basic parts to corresponding dirs

---
+++ Hive/HiveModel.Tests/EFUserRepositoryFixture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hive.Model.Repository;
+
+namespace Hive.Model.Repository.Tests
+{
+    [TestClass]
+    public class EFUserRepositoryFixture
+    {
+        [TestMethod]
+        public void ShouldFindUser()
+        {
+            var repo = new EFUserRepository();
+            var result = repo.Find("admin");
+            Assert.AreEqual("admin", result.Login);
+            Assert.AreEqual("admin", result.Password);
+        }
+    }
+}
---
+++ Hive/HiveModel/Repository/UserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hive.Model.Repository
+{
+    public interface UserRepository
+    {
+        Hive.Model.Domain.User Find(String login);
+    }
+}
